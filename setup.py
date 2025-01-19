b40e964dc28b33785813ee20ce42a2b0677fca3a
Dharansh Neema <dharanshneema@gmail.com>
2025-01-20 02:49:37 +0530

    Project setup done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version = "1.0.0",
+    author = "Dharansh Neema",
+    author_email = "dharanshneema@gmail.com",
+    packages = find_packages()
+)
